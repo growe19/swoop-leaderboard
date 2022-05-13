dc33c66513a1f0a707796a64fa70a9373975ae55
growe19 <gary.m.rowe@gmail.com>
2022-05-13 06:51:55 +0100

    Icons updated to use FontAwesome

--- modules/movement.js
+++ modules/movement.js
@@ -11,13 +11,13 @@ export default function movement(data, type) {
       let directionMove = "";
       if (m >= 1) {
         // Position change red, you've dropped places!
-        directionMove = "<span class=text-danger>&#9660;</span>";
+        directionMove = "<span class=text-danger><i class='fa-solid fa-chevron-down'></i></span>";
       } else if (m < 0 ) {
         // Position change green, you've overtaken cars!
-        directionMove = "<span class=text-success>&#9650;</span>";
+        directionMove = "<span class=text-success<i class='fa-solid fa-chevron-up'></i></span>";
       } else {
         // Position change static, you've maintained track position!
-        directionMove = "<span class=text-secondary>&#9655;</span>0";
+        directionMove = "<span class=text-secondary><i class='fa-solid fa-dash'></i></span> 0";
       }
       return directionMove + ' ' + data;
     }
