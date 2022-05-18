942a9f52515b2ec227e0dca41419e6a07065974d
Simon Gilhooly <simon.gilhooly@atom14.co.uk>
2022-05-18 14:50:03 +0100

    Calculate class leaders

--- modules/drawCallback.js
+++ modules/drawCallback.js
@@ -13,6 +13,7 @@ export default function drawCallback(settings) {
    *    establish the leader in each class and their time gap to the overall leader
    *    for each driver we can now use gapToLeader - classLeaderGapToLeader
    */
+  /*
   tabledata.forEach((tableRow) => {
     if (tableRow.isPlayer) {
       console.log("hello me");
@@ -24,7 +25,27 @@ export default function drawCallback(settings) {
 
     console.log(tableRow.class);
   });
+  */
+  const leaders = [];
+  api.rows().every(function (rowIdx, tableLoop, rowLoop) {
+    const car = this.data();
+    console.log(car);
+    if (car.isPlayer) {
+      console.log("hello me");
+    }
 
+    // actually this is always the first row!
+    if (car.racePosition === 1) {
+      console.log(`${car.currentDriver_FullName} is the leader`);
+    }
+
+    if (car.raceAppTag && car.raceAppTagPosition === 1) {
+      // console.log(car.currentDriver_FullName + ' is the ' + car.raceAppTag + ' leader');
+      leaders[car.raceAppTag] = car.id;
+    }
+  });
+
+  console.log('Class leaders: ', leaders);
   // lastDriveThroughTime   =   time driving through, stopping and exiting the pit lane
   // gapToLeader
 }
