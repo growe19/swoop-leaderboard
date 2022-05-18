7b9a9a6e4fd5a4e14716651ac817ef317f5b3066
Simon Gilhooly <simon.gilhooly@atom14.co.uk>
2022-05-18 12:45:57 +0100

    inital drawCallback

--- modules/drawCallback.js
+++ modules/drawCallback.js
@@ -1,18 +1,30 @@
+/**
+ * See https://datatables.net/reference/option/drawCallback
+ *
+ * @param {Object} settings see https://datatables.net/reference/type/DataTables.Settings
+ */
 export default function drawCallback(settings) {
-  // isPlayer
+  const api = this.api;
+  const tabledata = api.rows( {page:'current'} ).data();
+  console.log(tabledata);
+
+  /**
+   * calculate the gap to the class leader
+   *    establish the leader in each class and their time gap to the overall leader
+   *    for each driver we can now use gapToLeader - classLeaderGapToLeader
+   */
+  tabledata.forEach((tableRow) => {
+    if (tableRow.isPlayer) {
+      console.log("hello me");
+    }
+
+    if (tableRow.racePosition === 1) {
+      console.log(`${tableRow.currentDriver_FullName} is the leader`);
+    }
+
+    console.log(tableRow.class);
+  });
+
   // lastDriveThroughTime   =   time driving through, stopping and exiting the pit lane
   // gapToLeader
-
-  /*
-  var api = this.api();
-  console.log( api.rows( {page:'current'} ).data() );
-
-  var tabledata = api.rows( {page:'current'} ).data();
-  console.log(tabledata);
-  tabledata.forEach((row) => {
-    if ( row.isPlayer ) {
-      console.log("hello");
-    }
-  });
-  */
 }
